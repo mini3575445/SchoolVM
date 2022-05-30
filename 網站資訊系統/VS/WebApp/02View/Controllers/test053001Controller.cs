4fb6d2ea6dfbf710319b4612601aa658a40a2d82
unknown <C50119@VIEW.LOCAL>
2022-05-30 14:37:31 +0800

    SchoolVM List

---
+++ 網站資訊系統/VS/WebApp/02View/Controllers/test053001Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _02View.Models;
+namespace _02View.Controllers
+{
+    public class test053001Controller : Controller
+    {
+        // GET: test053001
+        public string EatApple<T>(T num)
+        {
+            
+            return "我吃了" + num + "個蘋果，資料型別為" + typeof(T);
+
+            
+        }
+
+        public void Main()
+        {
+            Response.Write(EatApple<int>(5)+"<br>");
+            Response.Write(EatApple<string>("6") + "<br>");
+            Response.Write(EatApple<float>(2.5f) + "<br>");
+        }
+    }
+}
