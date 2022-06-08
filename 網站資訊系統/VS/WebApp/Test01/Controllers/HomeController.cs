e3bb9a62f4664cebca1605440968784ddb246bbb
mini <mini3575445@gamil.com>
2022-06-08 21:34:42 +0800

    練習MVC Class陣列

--- 網站資訊系統/VS/WebApp/Test01/Controllers/HomeController.cs
+++ 網站資訊系統/VS/WebApp/Test01/Controllers/HomeController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Test01.Models;
+
 namespace Test01.Controllers
 {
     public class HomeController : Controller
     {
-        
+        // GET: Home
         public ActionResult Index()
         {
             return View();
@@ -23,7 +25,7 @@ namespace Test01.Controllers
                          select s;
             //擴充
             //var result = score.OrderByDescending(s => s);
-            foreach(var item in result)
+            foreach (var item in result)
             {
                 show += item + ",";
             }
@@ -46,7 +48,35 @@ namespace Test01.Controllers
             return show;
         }
 
+        public string LoginMember(string uid,string pwd)
+        {   
+            
 
+            string show = "";
 
+            //純class
+            //Member member = new Member() { UId = "A01", Pwd = "123", Name = "安安" };
+
+            //class陣列(裡面放member資料型態)
+            //class陣列(裡面放member資料型態)
+            Member[] members = new Member[3]
+            {
+            new Member(){ UId="A01",Pwd="123",Name="安安"},
+            new Member(){ UId="A02",Pwd="456",Name="咪路"},
+            new Member(){ UId="A03",Pwd="789",Name="爆米香"}
+            };
+            
+            var result = (from m in members
+                         where m.UId == uid && m.Pwd == pwd
+                         select m).FirstOrDefault();
+
+            if (result != null)
+                show = "歡迎" + result.Name + "登入";
+            else
+            {
+                show = "帳號密碼輸入錯誤";
+            }                      
+        return show;
+        }
     }
 }
