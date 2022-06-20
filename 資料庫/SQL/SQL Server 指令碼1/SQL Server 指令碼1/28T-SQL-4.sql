359cdbca81b1e861442299ab732121b0f2327739
School019 <C50119@VIEW.LOCAL>
2022-06-20 13:38:25 +0800

    0620

---
+++ 資料庫/SQL/SQL Server 指令碼1/SQL Server 指令碼1/28T-SQL-4.sql
@@ -0,0 +1,27 @@
+--¬yµ{±±¨î
+--while
+
+declare @i int=1,@sum int=0
+
+while @i<=100
+begin
+set @sum+=@i
+set @i+=1
+end
+print @sum
+go
+------------------------
+--*
+--**
+--***
+--****
+--*****
+
+declare @i int=1 , @result varchar(max)=''
+
+while @i<=50
+begin
+	set @result += '*'
+	set @i+=1
+	print @result
+end
---
+++ 資料庫/SQL/老師程式碼/sql/28T-SQL-4.sql
@@ -0,0 +1,19 @@
+--¬yµ{±±¨î
+--while
+
+declare @i int=1,@sum int=0
+
+while @i<=100
+begin
+	set @sum+=@i
+	set @i+=1
+end
+
+print @sum
+
+----------------------------------
+--*
+--**
+--***
+--****
+--*****
