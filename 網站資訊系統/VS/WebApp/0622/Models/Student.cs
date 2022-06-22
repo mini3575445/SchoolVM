5d011b3fd3465cd5ccef9225340215ae91ea7a86
School019 <C50119@VIEW.LOCAL>
2022-06-22 16:30:56 +0800

    0622

---
+++ 網站資訊系統/VS/WebApp/0622/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _0622.Models
+{
+    public class Student
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public int score { get; set; }
+
+
+        public Student() {
+            id = "";
+            name = "";
+            score = 0;            
+        }
+
+        public Student(string _id , string _name , int _score)
+        {
+            id = _id;
+            name = _name;
+            score = _score;
+        }
+
+
+        //修飾詞override，覆蓋繼承Object.ToString()這個內建方法
+        //所有的class都是繼承Object class
+        public override string ToString() 
+        {
+            //$，C#6新寫法，相當於以前的string.Format:
+            //return String.Format("id={0}，name={1}，score={2}",id,name,score);
+            return $"學號:{id}, 姓名:{name}, 分數:{score}.";
+        }
+
+    }
+}
