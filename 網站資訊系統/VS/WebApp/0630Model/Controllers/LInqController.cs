48cc4db2058807468ddf47ac967d824b4390e07c
mini <mini3575445@gamil.com>
2022-07-01 01:33:37 +0800

    Home IIS問題尚未解決

--- 網站資訊系統/VS/WebApp/0630Model/Controllers/LInqController.cs
+++ 網站資訊系統/VS/WebApp/0630Model/Controllers/LInqController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//1.引入Models
+using _0630Model.Models;
 
 namespace _0630Model.Controllers
 {
@@ -13,5 +15,27 @@ namespace _0630Model.Controllers
         {
             return View();
         }
+
+        //2.建立DB Context物件，於新增ADO時創建
+        NorthwindEntities db = new NorthwindEntities();
+
+        public string ShowEmployee() 
+        {
+            string show = "";
+
+            var result = from e in db.員工
+                         select e;
+
+            foreach (var item in result) 
+            {
+                show += "員工編號：" + item.員工編號 + "<br>";
+                show += "姓名：" + item.姓名 + "<br>";
+                show += "雇用日期：" + item.雇用日期 + "<br>";
+            }
+
+            return show;
+        }
     }
+
+   
 }
---
+++ 網站資訊系統/VS/WebApp/0701testIIS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_0701testIIS.MvcApplication" Language="C#" %>
