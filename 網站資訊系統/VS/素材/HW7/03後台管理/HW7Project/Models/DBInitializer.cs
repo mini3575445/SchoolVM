c19021b11e1e6b99cccbe23b4d6b5b3127a03049
SchoolMini <mini3575445@gmail.com>
2022-08-16 16:26:50 +0800

    0816

---
+++ 網站資訊系統/VS/素材/HW7/03後台管理/HW7Project/Models/DBInitializer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HW7Project.Models
+{
+    public class DBInitializer:DropCreateDatabaseAlways<HW7ProjectContext>
+    {
+        protected override void Seed(HW7ProjectContext context)
+        {
+            base.Seed(context);
+            ////////////////////////////////////////////
+            ///
+            List<Members> members = new List<Members>
+            {
+                new Members
+                {
+                   MemberName="莊孝為",
+                   MemberBirthday=Convert.ToDateTime("1981/7/9"),
+                   CreatedDate=DateTime.Today,
+                   Account="shaio",
+                   Password= "12345678"
+                }
+            };
+
+
+            members.ForEach(s => context.Members.Add(s));
+            context.SaveChanges();
+
+            List<Shippers> shippers = new List<Shippers>
+            {
+                new Shippers
+                {
+                   ShipVia="到店取貨"
+                },
+                new Shippers
+                {
+                   ShipVia="宅配到府"
+                },
+                new Shippers
+                {
+                   ShipVia="郵寄"
+                }
+            };
+
+            shippers.ForEach(s => context.Shippers.Add(s));
+            context.SaveChanges();
+
+            List<PayTypes> payType = new List<PayTypes>
+            {
+                new PayTypes
+                {
+                   PayTypeName="信用卡"
+                },
+                new PayTypes
+                {
+                   PayTypeName="Line Pay"
+                },
+                new PayTypes
+                {
+                   PayTypeName="貨到付款"
+                },
+                new PayTypes
+                {
+                   PayTypeName="到店取貨付款"
+                }
+            };
+
+            payType.ForEach(s => context.PayTypes.Add(s));
+            context.SaveChanges();
+
+            List<Employees> employees = new List<Employees>
+            {
+                new Employees
+                {
+                   EmployeeName="王立弘",
+                   CreatedDate=DateTime.Today,
+                   Account="admin",
+                   Password="P@ssw0rd"
+                }
+            };
+
+            employees.ForEach(s => context.Employees.Add(s));
+            context.SaveChanges();
+
+            /////////////////////////////////////////////////////////////////////////////
+
+
+        }
+
+
+    }
+}
---
+++ 網站資訊系統/VS/素材/HW7/04Filter/HW7Project/Models/Employees.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HW7Project.Models
+{
+    public class Employees
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage ="請填寫員工姓名")]
+        [StringLength(100,ErrorMessage = "員工姓名不超過100字")]
+        [DisplayName("員工姓名")]
+        public string EmployeeName { get; set; }
+
+        [DisplayName("建立日期")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+        public DateTime CreatedDate { get; set; }
+
+        [DisplayName("帳號")]
+        [Required(ErrorMessage = "請填寫帳號")]
+        [StringLength(20, ErrorMessage = "帳號不得超過20字")]
+        [RegularExpression("[A-Za-z][A-Za-z0-9]{4,19}", ErrorMessage = "帳號格式錯誤")]
+        public string Account { get; set; }
+
+        [DisplayName("密碼")]
+        [Required(ErrorMessage = "請填寫密碼")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "密碼最少8碼")]
+        [MaxLength(20, ErrorMessage = "密碼最多20碼")]
+        public string Password { get; set; }
+    }
+}
